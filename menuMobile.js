d7aa28409fb5f745d4a989ea839714e3b0427ca1
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-11-03 15:36:50 -0300

    Mais uma pág. adicionada.

--- menuMobile.js
+++ menuMobile.js
@@ -6,7 +6,9 @@ btn.addEventListener('click', () => {
     navbar.classList.toggle('active'); 
     if (btn.innerHTML == 'X') {
         btn.innerHTML = '<i class="fa-solid fa-bars"></i>'; 
+        document.body.style.overflow = 'auto'
     } else {
         btn.innerHTML = 'X'; 
+        document.body.style.overflow = 'hidden'
     }
 })
