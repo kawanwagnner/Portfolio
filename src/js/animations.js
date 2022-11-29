32fe0d2a321993b18e1b734d20a90d0a58d0349c
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-11-29 15:43:42 -0300

    em desenvolvimento.

--- src/js/animations.js
+++ src/js/animations.js
@@ -12,4 +12,4 @@ sr.reveal('.form_submit_email', { rotate: {x: 5, y: 20, z: 0}, duration: 2000 })
 
 sr.reveal('#logo', { duration: 2000 });
 
-sr.reveal('#navbar', { duration: 2000 });
+sr.reveal('.kawan-500', { duration: 2000 });
