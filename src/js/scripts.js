1ed71053974f06ec4372d7d3edf288aef2ab1d7d
Kawan Wagnner <kawanwagnner7@gmail.com>
2023-01-24 19:48:02 -0300

    Finish 1.0

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,3 +12,19 @@ btn.addEventListener('click', () => {
         document.body.style.overflow = 'hidden'
     }
 })
+
+/* Scroll Reveal; */
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#titulo', { 
+    rotate: {x: 5, y: 20, z: 0}, duration: 1000 
+})
+
+sr.reveal('.text-welcome', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+
+sr.reveal('#container-icons', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+
+sr.reveal('.form_submit_email', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+
+sr.reveal('#navbar', { duration: 1000 });
