bbede8831b0d5269be646fd9a944e6c1649a19da
Kawan Wagnner <kawanwagnner7@gmail.com>
2023-01-26 01:38:34 -0300

    Finish 1.1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,30 +1,39 @@
-const btn = document.querySelector('#btn-menu'); 
+/* Menu Navegation; */
+
+const btn = document.querySelector('#btn-menu');
 
 btn.addEventListener('click', () => {
-    const navbar = document.querySelector('#navbar'); 
+    const navbar = document.querySelector('#navbar');
+    const linksNavBar = document.querySelector('#list-navbar a[href^="#"]');
 
-    navbar.classList.toggle('active'); 
+    navbar.classList.toggle('active');
     if (btn.innerHTML == 'X') {
-        btn.innerHTML = '<i class="fa-solid fa-bars"></i>'; 
-        document.body.style.overflow = 'auto'
+        btn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+        document.body.style.overflowX = 'auto';
     } else {
-        btn.innerHTML = 'X'; 
-        document.body.style.overflow = 'hidden'
+        btn.innerHTML = 'X';
+        document.body.style.overflowX = 'hidden';
+    } 
+    
+    if (linksNavBar.addEventListener('click')) {
+        const navbarActive = document.querySelector('#navbar.active')
+        navbarActive.classList.remove('active')
+        btn.innerHTML = '<i class="fa-solid fa-bars"></i>'
     }
 })
 
 /* Scroll Reveal; */
 
-window.sr = ScrollReveal({ reset: true });
+// window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('#titulo', { 
-    rotate: {x: 5, y: 20, z: 0}, duration: 1000 
-})
+// sr.reveal('#titulo', { 
+//     rotate: {x: 5, y: 20, z: 0}, duration: 1000 
+// })
 
-sr.reveal('.text-welcome', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+// sr.reveal('.text-welcome', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
 
-sr.reveal('#container-icons', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+// sr.reveal('#container-icons', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
 
-sr.reveal('.form_submit_email', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+// sr.reveal('.form_submit_email', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
 
-sr.reveal('#navbar', { duration: 1000 });
+// sr.reveal('#navbar', { duration: 1000 });
