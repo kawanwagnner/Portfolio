208b8326c41a8709b5b7169059360627f334f719
Kawan Wagnner <kawanwagnner7@gmail.com>
2023-01-30 12:58:32 -0300

    Finish 1.3

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,33 +13,38 @@ btn.addEventListener('click', () => {
     } else {
         btn.innerHTML = '<i class="fa-solid fa-x"></i>';
         document.body.style.overflowX = 'hidden';
+        btn.style.top = '30px'
     }
     
     function removeMenu() {
         const navbarActive = document.querySelector('#navbar.active')
         navbarActive.classList.remove('active')
         btn.innerHTML = '<i class="fa-solid fa-bars"></i>'
-    }
+    };
 
-    linksNavBar[0].addEventListener('click', e => removeMenu())
-    linksNavBar[1].addEventListener('click', e => removeMenu())
-    linksNavBar[2].addEventListener('click', e => removeMenu())
-    linksNavBar[3].addEventListener('click', e => removeMenu())
-    linksNavBar[4].addEventListener('click', e => removeMenu())
+    linksNavBar[0].addEventListener('click', e => removeMenu());
+    linksNavBar[1].addEventListener('click', e => removeMenu());
+    linksNavBar[2].addEventListener('click', e => removeMenu());
+    linksNavBar[3].addEventListener('click', e => removeMenu());
+    linksNavBar[4].addEventListener('click', e => removeMenu());
 })
 
 /* Scroll Reveal; */
 
-// window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true });
 
-// sr.reveal('#titulo', { 
-//     rotate: {x: 5, y: 20, z: 0}, duration: 1000 
-// })
+sr.reveal('#titulo', { 
+    rotate: {x: 5, y: 20, z: 0}, duration: 2000 
+})
 
-// sr.reveal('.text-welcome', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+sr.reveal('#logo', { duration: 2000 });
 
-// sr.reveal('#container-icons', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+sr.reveal('.text-welcome', { rotate: {x: 5, y: 20, z: 0}, duration: 2000 });
 
-// sr.reveal('.form_submit_email', { rotate: {x: 5, y: 20, z: 0}, duration: 1000 });
+sr.reveal('#container-icons', { rotate: {x: 5, y: 20, z: 0}, duration: 2000 });
 
-// sr.reveal('#navbar', { duration: 1000 });
+sr.reveal('#container-text-iam', { rotate: {x: 5, y: 20, z: 0}, duration: 2000 });
+
+sr.reveal('.card', { rotate: {x: 5, y: 20, z: 0}, duration: 2000 });
+
+sr.reveal('#photo', { duration: 2000 });
