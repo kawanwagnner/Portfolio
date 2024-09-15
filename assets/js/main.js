e2e90183dff259b192d60d27a07ae81f55917498
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2024-09-15 02:19:15 -0300

    Atualizar o main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ showMenu("nav-toggle", "nav-menu");
 window.onload = function () {
   setTimeout(function () {
     document.getElementById("loading-screen").style.display = "none";
-  }, 3000);
+  }, 1000);
 };
 
 /*==================== REMOVE MENU MOBILE ====================*/
