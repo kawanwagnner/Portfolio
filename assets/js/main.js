c6d3a1707dbecc15e96177b944b7249c4214df9f
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-09-17 13:55:45 -0300

    fix: animation after of loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,22 @@ showMenu("nav-toggle", "nav-menu");
 window.onload = function () {
   setTimeout(function () {
     document.getElementById("loading-screen").style.display = "none";
+
+    // Dispara as animações apenas após o carregamento
+    const sr = ScrollReveal({
+      origin: "top",
+      distance: "60px",
+      duration: 2000,
+      delay: 200,
+      //     reset: true
+    });
+
+    sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
+    sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
+      delay: 400,
+    });
+    sr.reveal(".home__social-icon", { interval: 200 });
+    sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
   }, 3000);
 };
 
@@ -50,19 +66,3 @@ const scrollActive = () => {
   });
 };
 window.addEventListener("scroll", scrollActive);
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-  origin: "top",
-  distance: "60px",
-  duration: 2000,
-  delay: 200,
-  //     reset: true
-});
-
-sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
-sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
-  delay: 400,
-});
-sr.reveal(".home__social-icon", { interval: 200 });
-sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
